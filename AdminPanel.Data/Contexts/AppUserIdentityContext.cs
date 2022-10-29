d588c52468cb18abe7744400899f703b86015a4f
ncindric <cindrano1@gmail.com>
2022-10-30 00:23:09 +0200

    Added identity context

---
+++ AdminPanel.Data/Contexts/AppUserIdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdminPanel.Data.Contexts
+{
+    internal class AppUserIdentityContext : IdentityDbContext<AppUser>
+    {
+        public AppUserIdentityContext(DbContextOptions<AppUserIdentityContext> options)
+            : base(options)
+        {
+        }
+    }
+}
