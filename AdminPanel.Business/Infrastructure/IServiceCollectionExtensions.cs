96a0d361c58ba113f439aafc99b4c10dc5b024de
ncindric <cindrano1@gmail.com>
2022-10-29 18:11:36 +0200

    Added initialization for the business layer

---
+++ AdminPanel.Business/Infrastructure/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminPanel.Business.Infrastructure
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
