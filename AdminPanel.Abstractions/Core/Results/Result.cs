2ee1e3f46810a5bafae4dff8e56890b55aa534aa
ncindric <cindrano1@gmail.com>
2022-10-30 01:14:23 +0200

    Added Result types

---
+++ AdminPanel.Abstractions/Core/Results/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace AdminPanel.Abstractions.Core
+{
+    public class Result
+    {
+        internal Result(bool succeeded, string message)
+        {
+            Succeeded = succeeded;
+            Message = message;
+        }
+
+        public bool Succeeded { get; set; }
+
+        public string Message { get; set; }
+    }
+}
