609a0afb58678bc4b9fc56262c521b1298de6a6f
ncindric <cindrano1@gmail.com>
2022-10-29 18:16:39 +0200

    Added search initialization

---
+++ AdminPanel.Search/Infrastructure/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AdminPanel.Search.Infrastructure
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSearch(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
