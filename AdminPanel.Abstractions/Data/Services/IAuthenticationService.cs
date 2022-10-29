077e14f10f64d6636d47dfff43b55874ddcef26e
ncindric <cindrano1@gmail.com>
2022-10-30 01:14:34 +0200

    Added authentication service

---
+++ AdminPanel.Abstractions/Data/Services/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using AdminPanel.Abstractions.Core;
+
+namespace AdminPanel.Abstractions.Data.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Result> SignInAsync(string email, string password, bool isPersistent);
+
+        Task SignOutAsync();
+    }
+}
---
+++ AdminPanel.Data/Services/AuthenticationService.cs
@@ -0,0 +1,42 @@
+﻿using AdminPanel.Abstractions.Core;
+using AdminPanel.Abstractions.Core.Results;
+using AdminPanel.Abstractions.Data.Services;
+using AdminPanel.Data.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AdminPanel.Data.Services
+{
+    internal class AuthenticationService : IAuthenticationService
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthenticationService(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<Result> SignInAsync(string email, string password, bool isPersistent)
+        {
+            var result = await _signInManager.PasswordSignInAsync(
+                email,
+                password,
+                isPersistent,
+                true);
+
+            if (result.IsNotAllowed)
+            {
+                return ResultFactory.Failure("Login not allowed");
+            }
+
+            if (result.IsLockedOut)
+            {
+                return ResultFactory.Failure("Account locked out");
+            }
+
+            return ResultFactory.Success();
+        }
+
+        public async Task SignOutAsync() =>
+            await _signInManager.SignOutAsync();
+    }
+}
