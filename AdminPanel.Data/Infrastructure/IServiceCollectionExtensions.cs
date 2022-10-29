0289f4d7c5c7796761d67f2b791e9f18b4116468
ncindric <cindrano1@gmail.com>
2022-10-30 00:27:57 +0200

    Added entity framework configuration

--- AdminPanel.Data/Infrastructure/IServiceCollectionExtensions.cs
+++ AdminPanel.Data/Infrastructure/IServiceCollectionExtensions.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AdminPanel.Data.Contexts;
+using AdminPanel.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AdminPanel.Data.Infrastructure
 {
     public static class IServiceCollectionExtensions
     {
-        public static IServiceCollection AddData(this IServiceCollection services)
+        public static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<AppUserIdentityContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentityCore<AppUser>(
+                    options =>
+                    {
+                        options.SignIn.RequireConfirmedEmail = true;
+
+                        options.Password.RequiredLength = 8;
+
+                        options.User.RequireUniqueEmail = true;
+                    })
+                .AddEntityFrameworkStores<AppUserIdentityContext>();
 
             return services;
         }
