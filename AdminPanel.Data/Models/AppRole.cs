d5298f9bb0f3ce6e5f772f8fb2b4de035148a616
ncindric <cindrano1@gmail.com>
2022-10-31 00:06:05 +0100

    Added Permission and Role

---
+++ AdminPanel.Data/Models/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace AdminPanel.Data.Models
+{
+    internal class AppRole : IdentityRole
+    {
+        public IReadOnlyList<Permission> Permissions { get; set; } = Array.Empty<Permission>();
+    }
+}
---
+++ AdminPanel.Data/Models/Permission.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace AdminPanel.Data.Models
+{
+    internal class Permission
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public IReadOnlyList<AppRole> Roles { get; set; } = Array.Empty<AppRole>();
+    }
+}
