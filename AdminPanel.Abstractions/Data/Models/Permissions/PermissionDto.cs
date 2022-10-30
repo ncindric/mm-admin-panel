360e48bf8351a471e4cdee64ca52987900b02d4c
ncindric <cindrano1@gmail.com>
2022-10-31 00:29:25 +0100

    Added DTOs

---
+++ AdminPanel.Abstractions/Data/Models/Permissions/PermissionDto.cs
@@ -0,0 +1,13 @@
+﻿using AdminPanel.Abstractions.Data.Models.Roles;
+
+namespace AdminPanel.Abstractions.Data.Models.Permissions
+{
+    public class PermissionDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public IReadOnlyList<RoleDto> Roles { get; set; } = Array.Empty<RoleDto>();
+    }
+}
