19cc806a5c96efa69695b1d9910a05bfc2dd320e
ncindric <cindrano1@gmail.com>
2022-10-31 02:07:26 +0100

    Added permission authorization attribute

---
+++ AdminPanel.Web/Infrastructure/AuthorizePermissionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AdminPanel.Web.Infrastructure
+{
+    public sealed class AuthorizePermissionAttribute : Attribute
+    {
+        public AuthorizePermissionAttribute(string permission)
+        {
+            Permission = permission;
+        }
+
+        public string Permission { get; }
+    }
+}
