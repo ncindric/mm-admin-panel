f9e8c3c32987df7e0e71777f7d542fe28649827c
ncindric <cindrano1@gmail.com>
2022-10-31 02:07:41 +0100

    Added permission filter

---
+++ AdminPanel.Web/Infrastructure/PermissionFilter.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using AdminPanel.Abstractions.Data.Services;
+using AdminPanel.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AdminPanel.Web.Infrastructure
+{
+    public class PermissionFilter : IActionFilter
+    {
+        private readonly IPermissionService _permissionService;
+
+        public PermissionFilter(IPermissionService permissionService)
+        {
+            _permissionService = permissionService;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionDescriptor is not ControllerActionDescriptor controllerActionDescriptor)
+            {
+                return;
+            }
+
+            var permissionAttribute = controllerActionDescriptor.MethodInfo
+                .GetCustomAttribute<AuthorizePermissionAttribute>();
+            
+            // fallback to controller attribute if not specified on method
+            permissionAttribute ??= controllerActionDescriptor.ControllerTypeInfo
+                .GetCustomAttribute<AuthorizePermissionAttribute>();
+            if (permissionAttribute == null)
+            {
+                return;
+            }
+
+            var permission = _permissionService.Get()
+                .FirstOrDefault(p => string.Equals(p.Name, permissionAttribute.Permission));
+            if (permission == null)
+            {
+                return;
+            }
+
+            if (permission.Roles.All(r => !context.HttpContext.User.IsInRole(r.Name)))
+            {
+                context.Result = context.HttpContext.User.Identity?.IsAuthenticated ?? false
+                    ? new ForbidResult()
+                    : new ChallengeResult();
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        { }
+    }
+}
