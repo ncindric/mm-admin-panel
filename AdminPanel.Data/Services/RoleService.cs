fc4dffeb6b9c674c957943ab8c9fc1a02edbe2ce
ncindric <cindrano1@gmail.com>
2022-10-31 01:39:15 +0100

    Added role service

---
+++ AdminPanel.Data/Services/RoleService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdminPanel.Abstractions.Data.Models.Roles;
+using AdminPanel.Abstractions.Data.Services;
+using AdminPanel.Data.Contexts;
+
+namespace AdminPanel.Data.Services
+{
+    internal class RoleService : IRoleService
+    {
+        private readonly AppUserIdentityContext _context;
+
+        public RoleService(AppUserIdentityContext context)
+        {
+            _context = context;
+        }
+
+        public IReadOnlyList<RoleDto> Get()
+        {
+            return _context.Roles.Select(r => new RoleDto
+            {
+                Id = r.Id,
+                Name = r.Name,
+            }).ToList();
+        }
+    }
+}
