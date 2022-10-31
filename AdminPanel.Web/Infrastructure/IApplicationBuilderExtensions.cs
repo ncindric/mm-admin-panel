6f2c3405a3837dc9dddb03686c6cb5477675d857
ncindric <cindrano1@gmail.com>
2022-10-31 23:06:00 +0100

    Added extension for the initialization of the permissions

---
+++ AdminPanel.Web/Infrastructure/IApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.Abstractions.Data.Services;
+
+namespace AdminPanel.Web.Infrastructure
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static void UsePermissions(this IApplicationBuilder app)
+        {
+            var scope = app.ApplicationServices.CreateScope();
+            var permissionInitializer = scope.ServiceProvider.GetRequiredService<IPermissionInitializer>();
+            permissionInitializer.Initialize().GetAwaiter().GetResult();
+        }
+    }
+}
