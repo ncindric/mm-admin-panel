1f427913e9310f6264cca34912b244d808cdfac7
ncindric <cindrano1@gmail.com>
2022-10-31 23:04:20 +0100

    Added exposure of all roles and all permissions

--- AdminPanel.Shared/Constants/RoleNames.cs
+++ AdminPanel.Shared/Constants/RoleNames.cs
@@ -1,8 +1,15 @@
-﻿namespace AdminPanel.Shared.Constants
+﻿using System.Collections;
+
+namespace AdminPanel.Shared.Constants
 {
     public static class RoleNames
     {
         public const string Administrator = "Administrator";
         public const string Editor = "Editor";
+
+        public static readonly IReadOnlyList<string> AllRoles = new List<string>
+        {
+            Administrator,
+        };
     }
 }
