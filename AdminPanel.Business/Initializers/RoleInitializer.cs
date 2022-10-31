8995dd8fa0d2bf21a6aa1968ae09df098637f975
ncindric <cindrano1@gmail.com>
2022-10-31 23:20:08 +0100

    Added new initializers for roles and permissions

---
+++ AdminPanel.Business/Initializers/RoleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using AdminPanel.Abstractions.Core.Initialization;
+using AdminPanel.Abstractions.Data.Services;
+
+namespace AdminPanel.Business.Initializers
+{
+    internal class RoleInitializer : IInitializer
+    {
+        private readonly IRoleSeedService _roleSeedService;
+
+        public RoleInitializer(IRoleSeedService roleSeedService)
+        {
+            _roleSeedService = roleSeedService;
+        }
+
+        public void Initialize() => _roleSeedService.Seed().GetAwaiter().GetResult();
+    }
+}
